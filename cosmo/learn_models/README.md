fc8d1d18777c4b90fc2ab3435146bb3736ea8c75
Yihui (Ray) Ren <yihui.ren@gmail.com>
2019-07-29 15:25:40 -0400

    add 3-way data split

--- cosmo/learn_models/README.md
+++ cosmo/learn_models/README.md
@@ -26,3 +26,42 @@ rank_3_has_acc_per_class | 1.0000    |    0.9000   |     0.6364   |     0.4821  
 per_class_opt1           | 1.0000    |    0.9667   |     0.9091   |     0.9464   |     0.7143   |     1.0000
 per_class_opt2           | 1.0000    |    0.9000   |     0.9455   |     0.7679   |     1.0000   |     1.0000
 
+
+
+
+rank 1 has acc 0.9711538461538461
+rank 2 has acc 0.8878205128205128
+rank 3 has acc 0.9583333333333334
+rank 5 has acc 0.8461538461538461
+rank 4 has acc 0.8461538461538461
+rank 5 has acc_per_class 1.0000 0.9333 0.3818 0.8214 1.0000 1.0000
+rank 4 has acc_per_class 1.0000 1.0000 0.3091 0.8571 0.9592 1.0000
+rank 7 has acc 0.9743589743589743
+rank 6 has acc 0.8365384615384616
+rank 7 has acc_per_class 1.0000 0.9833 0.9455 1.0000 0.9184 1.0000
+rank 6 has acc_per_class 1.0000 0.6333 0.7636 0.7857 0.9184 1.0000
+opt 1 acc =  0.9519230769230769
+opt 2 acc =  0.9807692307692307
+tot per class [49. 60. 55. 56. 49. 43.]
+per class opt1 =  1.0000 1.0000 0.8182 0.9643 0.9388 1.0000
+per class opt2 =  1.0000 0.9833 0.9455 0.9821 0.9796 1.0000
+rank 0 has acc 0.6955128205128205
+rank: 1 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 5 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 2 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 7 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 4 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 6 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 0 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+rank: 3 train tot per_class [ 84.  94.  91.  90. 100. 102.]
+
+rank 0 has acc_per_class 0.9184 0.4500 0.9273 0.5893 0.3673 1.0000
+rank 1 has acc_per_class 0.9796 1.0000 0.9091 1.0000 0.9388 1.0000
+rank 2 has acc_per_class 1.0000 0.8667 0.5636 0.9643 0.9796 1.0000
+rank 3 has acc_per_class 1.0000 0.9167 1.0000 1.0000 0.8367 1.0000
+rank 4 has acc_per_class 1.0000 1.0000 0.3091 0.8571 0.9592 1.0000
+rank 5 has acc_per_class 1.0000 0.9333 0.3818 0.8214 1.0000 1.0000
+rank 6 has acc_per_class 1.0000 0.6333 0.7636 0.7857 0.9184 1.0000
+rank 7 has acc_per_class 1.0000 0.9833 0.9455 1.0000 0.9184 1.0000
+per class opt1        =  1.0000 1.0000 0.8182 0.9643 0.9388 1.0000
+per class opt2        =  1.0000 0.9833 0.9455 0.9821 0.9796 1.0000
--- cosmo/learn_models/cosmo_data.py
+++ cosmo/learn_models/cosmo_data.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 import pandas as pd
 import numpy as np
+import collections
 from pathlib import Path
 
 ## example usage:
@@ -14,12 +15,12 @@ class Cosmo3D(Dataset):
         """
         super(Cosmo3D, self).__init__()
         base_folder = Path(base_folder)
-        assert(base_folder.exists() and base_folder.is_dir())
+        assert (base_folder.exists() and base_folder.is_dir())
         if train: 
-            assert(base_folder/'train.csv').exists()
+            assert (base_folder/'train.csv').exists()
             self.df = pd.read_csv(base_folder/'train.csv')
         else:
-            assert(base_folder/'test.csv').exists()
+            assert (base_folder/'test.csv').exists()
             self.df = pd.read_csv(base_folder/'test.csv')
         self.base_folder      = base_folder
         self.train            = train
@@ -51,24 +52,65 @@ def get_data(data_dir, bsz, num_workers=8, pin_memory=True,\
         NOTE: to make sure the member models of an ensemble have the same subset,
         a random seed should need to be specified.
     """
+    def shuffle_idx():
+        idx = np.arange(len(train_data))
+        np_rnd_state = np.random.get_state()
+        np.random.seed(seed)
+        np.random.shuffle(idx)
+        np.random.set_state(np_rnd_state)
+        return idx
+
     # data_dir = '/home/yren/data/cosmo_data/npy/'
-    assert(Path(data_dir).exists() and Path(data_dir).is_dir())
+    assert (Path(data_dir).exists() and Path(data_dir).is_dir())
     train_data = Cosmo3D(data_dir, transform=np_norm)
     test_data = Cosmo3D(data_dir, train=False, transform=np_norm)
     if not amount:
+        # return all training data and all testing data
         train_loader = DataLoader(train_data, batch_size=bsz, \
                                 shuffle=True, num_workers=num_workers, \
                                 pin_memory=pin_memory)
         test_loader = DataLoader(test_data, batch_size=2*bsz)
-    else:
+        return train_loader, test_loader
+    elif not isinstance(amount, collections.Sequence) or len(amount) == 1:
+        # amount is a number or a sequence with one element
+        if isinstance(amount, collections.Sequence): amount = amount[0]
         if amount < 1: amount = int(len(train_data)*amount)
-        assert( 0 < amount < len(train_data) )
-        idx = np.arange(len(train_data))
-        np.random.seed(seed)
-        np.random.shuffle(idx)
+        assert ( 0 < amount < len(train_data) )
+        idx = shuffle_idx()
+        ## idx = np.arange(len(train_data))
+        ## np_rnd_state = np.random.get_state()
+        ## np.random.seed(seed)
+        ## np.random.shuffle(idx)
+        ## np.random.set_state(np_rnd_state)
         train_loader = DataLoader(train_data, batch_size=bsz, \
                                   sampler=SubsetRandomSampler(idx[:amount]), \
                                   pin_memory=pin_memory)
         test_loader = DataLoader(test_data, batch_size=2*bsz)
-
-    return train_loader, test_loader
+        return train_loader, test_loader
+    else:
+        # first number for train, 
+        # second number for dev, and
+        # third number (if present) for test
+        train_amount, dev_amount = amount[0], amount[1]
+        if train_amount < 1 : train_amount = int(len(train_data)*train_amount)
+        if dev_amount < 1   : dev_amount = int(len(train_data)*dev_amount)
+        print(train_amount, dev_amount, len(train_data) )
+        assert train_amount + dev_amount <= len(train_data), \
+               "Train Amount : {0} and Dev Amount : {1} is greater than the \
+               Data Size : {2}".format(train_amount, dev_amount,
+                                       len(train_data) ) 
+        idx = shuffle_idx()
+        #  idx = np.arange(len(train_data))
+        #  np_rnd_state = np.random.get_state()
+        #  np.random.seed(seed)
+        #  np.random.shuffle(idx)
+        #  np.random.set_state(np_rnd_state)
+        train_loader = DataLoader(train_data, batch_size=bsz, \
+                           sampler=SubsetRandomSampler(idx[:train_amount]), \
+                           pin_memory=pin_memory)
+        dev_loader   = DataLoader(train_data, batch_size=2*bsz, \
+                           sampler=SubsetRandomSampler(\
+                               idx[train_amount:(train_amount+dev_amount)]), \
+                           pin_memory=pin_memory)
+        test_loader = DataLoader(test_data, batch_size=2*bsz)
+        return train_loader, dev_loader, test_loader
